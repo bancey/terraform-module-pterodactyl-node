743b422b3288364bcdbde6530dc28c81c5078ebd
Alex Bance <alex.bance@kainos.com>
2022-11-14 10:38:57 +0000

    fix: incorrect url to wings.service

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -18,5 +18,5 @@ mkdir -p /etc/pterodactyl
 curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
 chmod u+x /usr/local/bin/wings
 
-curl -L -o /etc/systemd/system/wings.service "https://github.com/bancey/terraform-module-pterodactyl-node/blob/a8218ae0c87f81fb43b7dce54ae85264c09eb5ad/provision/wings.service"
+curl -L -o /etc/systemd/system/wings.service "https://raw.githubusercontent.com/bancey/terraform-module-pterodactyl-node/develop/provision/wings.service"
 systemctl enable wings
