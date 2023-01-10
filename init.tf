3f754484ac5d490f6cdfa8644703787dab35b8f9
Alex Bance <alex.bance@live.co.uk>
2023-01-10 13:05:31 +0000

    fix: updated versions

--- init.tf
+++ init.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.38.0"
+      version = ">= 3.38.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.0"
+      version = ">= 3.4.3"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "4.0"
+      version = ">= 4.0.4"
     }
   }
 }
